0ec46f22cbdaa2ef8dbb183278aef7f0f9bed1bc
Malik Firose <firosemalik@gmail.com>
2025-07-05 20:37:05 +1000

    circuit breaker and open telementry distributed tracing

--- src/main/java/org/malik/micro/currency_conversion_service/controller/CurrencyConversionController.java
+++ src/main/java/org/malik/micro/currency_conversion_service/controller/CurrencyConversionController.java
@@ -1,21 +1,18 @@
 package org.malik.micro.currency_conversion_service.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.logging.log4j.util.Strings;
 import org.malik.micro.currency_conversion_service.api.CurrencyConversion;
 import org.malik.micro.currency_conversion_service.api.CurrencyExchange;
 import org.malik.micro.currency_conversion_service.client.CurrencyExchangeClient;
-import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
-import java.net.http.HttpHeaders;
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,17 +22,24 @@ public class CurrencyConversionController {
     private final CurrencyExchangeClient currencyExchangeClient;
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    @RateLimiter(name = "default", fallbackMethod = "fallBackResponse")
     public CurrencyConversion calculateConversion(@PathVariable String from,
                                                   @PathVariable String to,
                                                   @PathVariable int quantity,
                                                   HttpServletRequest request) {
+        log.info("currency conversion called");
         final CurrencyExchange exchangeValue = currencyExchangeClient.getExchangeValue(from, to);
+        log.info("currency exchange call completed {} -> {}", exchangeValue.getFrom(), exchangeValue.getTo());
 
         request.getHeaderNames().asIterator().forEachRemaining(h -> log.info("values : {}", h.toLowerCase()));
 
         return CurrencyConversion.builder().from(from).to(to).conversion(exchangeValue.getConversionMultiple())
                 .quantity(quantity).finalAmount(exchangeValue.getConversionMultiple().multiply(new BigDecimal(quantity)))
                 .environment(exchangeValue.getEnvironment()).build();
+    }
+
+    public CurrencyConversion fallBackResponse() {
+        return CurrencyConversion.builder().environment("fallback activated").build();
     }
 
 }
